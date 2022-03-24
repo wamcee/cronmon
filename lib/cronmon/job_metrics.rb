2b6db92f145cb4ca793c8e3ecf169a8d743156b9
wamcee <vamshidhar.reddy@meisterlabs.com>
2022-03-24 15:41:58 +0100

    Add the loggers to log data in the db

--- lib/cronmon/job_metrics.rb
+++ lib/cronmon/job_metrics.rb
@@ -10,12 +10,27 @@ module JobMetrics
     prepend SidekiqCallbacks
 
     around_perform do |job, block|
-      cron = Cron.find_by(action: job.class.to_s)
-      @cron_log = CronLog.create(cron: cron, started_at: Time.now, job_id: jid)
+      create_cron(job)
+      create_cron_log(jid)
+
       block.call
-      @cron_log.update(ended_at: Time.now)
-    rescue StandardError => e
-      @cron_log.update(error_log: e, ended_at: Time.now)
+
+      update_cron_log
+    rescue StandardError => err
+      update_cron_log(err)
+    end
+
+    def create_cron_log(jid)
+      @cron_log ||= CronLog.create(cron: @cron, started_at: Time.now, job_id: jid)
+    end
+
+    def create_cron(job)
+      @cron ||= Cron.find_by(action: job.class.to_s)
+    end
+
+    def update_cron_log(err = nil)
+      @cron_log.update(error_log: err, ended_at: Time.now)
     end
   end
 end
+
